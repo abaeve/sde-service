e6321cc61c194cc013295e699d574d0047a2dd3e
Matt Maurer <maurer.it@gmail.com>
2017-04-27 14:22:20 -0400

    Initial service main function functionality
    Shouldn't be much else besides something to happen inside the initialize function.

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	proto "github.com/abaeve/sde-service/proto"
+	"github.com/abaeve/services-common/config"
+	"github.com/abaeve/sde-service/handler"
+)
 
+var version string = "1.0.0"
+
+func main() {
+	service := config.NewService(version, "sde-service", initialize)
+
+	proto.RegisterTypeQueryHandler(service.Server(), handler.TypeQueryHandler)
+
+	service.Run()
+}
+
+func initialize(config *config.Configuration) error {
+	return nil
 }
